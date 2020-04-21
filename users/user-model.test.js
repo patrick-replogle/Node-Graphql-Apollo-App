449a4ffccb066cc0c6c22514e5bb5bd26f13f09a
patrick-replogle <patrickr1138@gmail.com>
2020-04-20 21:59:27 -0700

    added tests

--- users/user-model.test.js
+++ users/user-model.test.js
@@ -1,42 +1,56 @@
-// let userModel = require("./user-model.js");
-// let db = require("../database/dbConfig.js");
+let userModel = require("./user-model.js");
+let db = require("../database/dbConfig.js");
 
-// describe("User models", () => {
-//   let userCount = 0;
-//   beforeAll(async () => {
-//     userCount = await (await db("users")).length;
-//   });
+const newUser = {
+  email: String(Math.random()),
+  password: "secret",
+  firstName: "John",
+  lastName: "Doe",
+  location: "Portland",
+  gender: "male",
+};
 
-//   test("find all users", async () => {
-//     const users = await userModel.find();
-//     expect(users.length).toEqual(userCount);
-//     expect(users);
-//   });
+const updatedUser = {
+  email: String(Math.random()),
+  password: "stillsecret",
+  firstName: "Jane",
+  lastName: "Doe",
+  location: "Boston",
+  gender: "female",
+};
 
-//   //   test("find user by id", async () => {
-//   //     if (userCount > 0) {
-//   //       const user = await userModel.findById(userCount - 1);
-//   //       expect(user.id).toEqual(userCount - 1);
-//   //     } else {
-//   //       expect(user).toBeUndefined();
-//   //     }
-//   //   });
+describe("User models", () => {
+  let createdUserId = 0;
+  let userCount = 0;
+  beforeAll(async () => {
+    userCount = await (await db("users")).length;
+  });
 
-//   //   test("adds a user", async () => {
-//   //     const newUser = {
-//   //       email: String(Math.random()),
-//   //       password: "secrets",
-//   //       firstName: "John",
-//   //       lastName: "Doe",
-//   //       location: "Portland",
-//   //       gender: "Male",
-//   //     };
-//   //     const found = await userModel.findBy({ email: newUser.email });
-//   //     if (!found) {
-//   //       const added = await userModel.add(newUser);
-//   //       expect(userCount).toBe(userCount + 1);
-//   //     }
-//   //   });
-// });
+  test("find all users", async () => {
+    const users = await userModel.find();
+    expect(users.length).toEqual(userCount);
+  });
 
-test("hello", () => {});
+  test("adds a user", async () => {
+    const added = await userModel.add(newUser);
+    createdUserId = added.id;
+    const users = await db("users").select();
+    expect(users).toHaveLength(userCount + 1);
+  });
+
+  test("find user by id", async () => {
+    const user = await userModel.findById(createdUserId);
+    expect(user.firstName).toEqual("John");
+  });
+
+  test("added user is updated", async () => {
+    const updated = await userModel.update(createdUserId, updatedUser);
+    expect(updated.firstName).toBe("Jane");
+  });
+
+  test("removes the added user", async () => {
+    const deleted = await userModel.remove(createdUserId);
+    const users = await db("users");
+    expect(users).toHaveLength(userCount);
+  });
+});
